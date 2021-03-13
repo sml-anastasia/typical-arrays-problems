14c305aac54bd0f14ba6877a4d6da7e6f4aa7a67
anastasia <smolina.anastasia.14@gmail.com>
2021-03-13 18:13:25 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array.length == 0) {
+    return Math.min.apply(null, array);
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array.length == 0) {
+    return Math.max.apply(null, array);
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array.length == 0) {
+    return array.reduce((a, b) => a + b) / array.length;
+  } else {
+    return 0;
+  }
 }
+
+min(1, 2, 3, 4);
+max(1, 2, 3, 4);
+avg(1, 2, 3, 4);
